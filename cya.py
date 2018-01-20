f5f8a5468955cdb19e4052a989f524585dcb5fdd
Daniel <dstambl2@jhu.edu>
2018-01-20 15:43:46 -0500

    Successfully parsed JSON

--- cya.py
+++ cya.py
@@ -1,6 +1,8 @@
 import spacy
 import json
 
+
+#loads and tolkenizes
 nlp = spacy.load('en')
 
 to_process = input('enter some string: ')
@@ -13,15 +15,16 @@ for token in doc:
     pair = (token.text, token.pos_)
     words.append(pair)
 
-
 for x in words: 
 	print(x)
 
+#loads JSON into array
 data = json.load(open('keywords.json'))
 
 
+nounList = data['nouns']
+adjList = data['adjective']
 
-#loads the json keywords file into a dict
-for key, value in data.items():
-		for k, v in value.items():
-			print(k)
+print(nounList)
+
+	
