60da6441466ab96c8861749435e43a4aba889f20
Daniel <dstambl2@jhu.edu>
2018-01-20 10:33:35 -0500

    small cya updates

--- cya.py
+++ cya.py
@@ -6,5 +6,12 @@ to_process = input('enter some string: ')
 
 doc = nlp(to_process)
 
+words = []
+
 for token in doc:
-    print(token.text, token.pos_)
+    pair = (token.text, token.pos_)
+    words.append(pair)
+
+
+for x in words: 
+	print(x)
